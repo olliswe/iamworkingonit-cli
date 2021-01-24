d06870cc5882c6361a29aefa791776274c96e27c
Oliver Iyer <34507469+olliswe@users.noreply.github.com>
2021-01-24 13:56:11 +0100

    feat: refactoring status to be base command (#2)
    * refactor status
    
    * remove status

--- README.md
+++ README.md
@@ -28,12 +28,33 @@ USAGE
 <!-- usagestop -->
 # Commands
 <!-- commands -->
+* [`workingon [COMMENT]`](#workingon-comment)
 * [`workingon help [COMMAND]`](#workingon-help-command)
 * [`workingon list`](#workingon-list)
 * [`workingon login`](#workingon-login)
 * [`workingon logout`](#workingon-logout)
 * [`workingon show`](#workingon-show)
-* [`workingon status [COMMENT]`](#workingon-status-comment)
+
+## `workingon [COMMENT]`
+
+Update or clear your status
+
+```
+USAGE
+  $ workingon [COMMENT]
+
+ARGUMENTS
+  COMMENT  Describe your current status
+
+OPTIONS
+  --clear  Clear you status
+
+EXAMPLES
+  $ workingon "Refactoring Dashboard"
+  $ workingon --clear
+```
+
+_See code: [src/commands/index.ts](https://github.com/olliswe/iamworkingonit-cli/blob/v0.0.0/src/commands/index.ts)_
 
 ## `workingon help [COMMAND]`
 
@@ -120,25 +141,4 @@ EXAMPLE
 ```
 
 _See code: [src/commands/show.ts](https://github.com/olliswe/iamworkingonit-cli/blob/v0.0.0/src/commands/show.ts)_
-
-## `workingon status [COMMENT]`
-
-Update or clear your status
-
-```
-USAGE
-  $ workingon status [COMMENT]
-
-ARGUMENTS
-  COMMENT  Describe your current status
-
-OPTIONS
-  --clear  Clear you status
-
-EXAMPLES
-  $ workingon status "Refactoring Dashboard"
-  $ workingon status --clear
-```
-
-_See code: [src/commands/status.ts](https://github.com/olliswe/iamworkingonit-cli/blob/v0.0.0/src/commands/status.ts)_
 <!-- commandsstop -->
---
+++ src/commands/index.ts
@@ -0,0 +1,71 @@
+import { Command, flags } from '@oclif/command'
+import cli from 'cli-ux'
+import * as inquirer from 'inquirer'
+import { handleError } from '../utils'
+import { STD_ERRORS } from '../config'
+
+export default class Status extends Command {
+    static description = 'Update or clear your status'
+
+    static examples = [
+        `$ workingon "Refactoring Dashboard"`,
+        `$ workingon --clear`,
+    ]
+
+    static flags = { clear: flags.boolean({ description: 'Clear you status' }) }
+
+    static args = [
+        {
+            name: 'comment',
+            required: false,
+            description: 'Describe your current status',
+        },
+    ]
+
+    async run() {
+        const { args } = this.parse(Status)
+        const { flags } = this.parse(Status)
+
+        if (flags.clear) {
+            cli.action.start('Clearing your status')
+            //TODO: Clear status
+
+            cli.action.stop('Done')
+            return
+        }
+
+        const status = args?.comment
+        if (!status) {
+            this.error(`You need to specify a status update!
+e.g. $ workingon "Refactoring tests"`)
+            return
+        }
+
+        const statusWords = status.split(' ')
+        if (statusWords.length === 1) {
+            this.log(
+                `Sorry, the command "$ workingon ${status}" was not recognized`
+            )
+            let responses = await inquirer.prompt([
+                {
+                    name: 'isUpdate',
+                    message: `Do you wish to update your status to "${status}"`,
+                    type: 'confirm',
+                },
+            ])
+            const isUpdate = responses.isUpdate
+            if (!isUpdate) {
+                this.error(handleError(STD_ERRORS.HELP_ERROR))
+                return
+            }
+        }
+
+        cli.action.start(`Setting your status to
+"${status}"
+      `)
+        //TODO: Update status
+
+        cli.action.stop('Done')
+        return
+    }
+}
